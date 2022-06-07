ce5a0fe98192111c2a8b615fe154fc580ac457aa
HttpCaldeira <gui-caldeira2011@hotmail.com>
2022-06-07 16:59:24 -0300

    Logica Para imagem

--- admin/post-atualiza.php
+++ admin/post-atualiza.php
@@ -6,6 +6,30 @@ $posts = lerPosts($conexao, $_SESSION['id'], $_SESSION['tipo']);
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 $dados = lerUmPost($conexao, $id);
 
+if(isset($_POST['atualizar'])){
+  $titulo = filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_SPECIAL_CHARS);
+	$texto = filter_input(INPUT_POST, 'texto', FILTER_SANITIZE_EMAIL);
+	$resumo = filter_input(INPUT_POST, 'resumo', FILTER_SANITIZE_SPECIAL_CHARS);
+	
+  //Lógica para a imagem Se o campo da imagem do formulario estiver vazio, então significa que o usuário não mudou a imagem.
+  if(empty($_POST['imagem'])){
+    $imagem = $dados['imagem']; //Manter a imagem Já Existente no banco
+  } else {
+
+  /* Caso Contrario, se o usuário alterou a imagem no campo imagem, precisaremos verificar se a imagem existe no banco de dados. */
+
+  $imagem = verificaSenha($_POST['imagem'], $dados['imagem']);
+  }
+   
+  
+  atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo);
+
+	header("location:usuarios.php");
+  // echo "banco ".$dados['senha'];
+  // echo "<br>";
+  // echo "formulario ".$senha;
+}
+
 ?>
        
 <div class="row">
